898a26ed8afdd531f8577ef15b959b1e6d81708d
daniellorenzen <dani141d@stud.kea.dk>
2023-04-18 13:12:07 +0200

    fuck off

--- src/main/java/com/example/jwtspringsecurity/model/User.java
+++ src/main/java/com/example/jwtspringsecurity/model/User.java
@@ -1,6 +1,9 @@
 package com.example.jwtspringsecurity.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,11 +18,18 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @NotBlank
+    @Size(max = 20)
     private String username;
 
+    @NotBlank
+    @Size(max = 50)
+    @Email
     private String email;
 
+    @NotBlank
+    @Size(max = 120)
     private String password;
 
     @ManyToMany(fetch = FetchType.LAZY)
